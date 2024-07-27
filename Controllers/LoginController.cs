e00a4cb6ac0c141f55c1993ad76303454c479586
Oliver Fiedler <o.lyngholm@gmail.com>
2024-07-27 23:12:41 +0200

    A lot of small stuff for tomorrow

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -1,5 +1,8 @@
 
 
+using farmaatte_api.DTOs;
+using farmaatte_api.Models;
+using farmaatte_api.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,18 +12,43 @@ namespace farmaatte_api.Controllers;
 public class LoginController : V1ControllerBase
 {
 
+    private readonly FarmaatteDbContext _context;
     private readonly ILogger<LoginController> _logger;
 
-    public LoginController(ILogger<LoginController> logger)
+    private readonly HashingService _hashingService;
+
+    public LoginController(ILogger<LoginController> logger, FarmaatteDbContext context, HashingService hashingService)
     {
+        _context = context;
         _logger = logger;
+        _hashingService = hashingService;
     }
 
     [HttpPost]
     [AllowAnonymous]
+    [Consumes("application/json")]
     [Produces("application/json")]
-    public IActionResult Login()
+    public async Task<IActionResult> Login([FromBody] LoginDTO dto)
     {
-        return Ok();
+        var user = await _context.Users.FindAsync(dto.Username);
+        if (user == null)
+        {
+            Thread.Sleep(3000);
+            return Unauthorized();
+        }
+        else
+        {
+            var hash = _hashingService.HashString(user.Pwhash);
+            if (dto.Password != hash)
+            {
+                Thread.Sleep(3000);
+                return Unauthorized();
+            }
+            else
+            {
+                // TODO: issue token and return okay
+                return Ok(user.Userid);
+            }
+        }
     }
 }
---
+++ Services/HashingService.cs
@@ -0,0 +1,15 @@
+
+
+namespace farmaatte_api.Services;
+
+public class HashingService
+{
+
+    public HashingService() { }
+
+    public string HashString(string s)
+    {
+        // Do some stuff
+        return "";
+    }
+}
